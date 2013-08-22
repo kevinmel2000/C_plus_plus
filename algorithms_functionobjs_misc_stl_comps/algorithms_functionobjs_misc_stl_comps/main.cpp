e24b432faec25fd761e655277c88cd2bd9822a31
Sudhee <sudhee@udel.edu>
2013-08-21 22:32:12 -0400

    images

---
+++ algorithms_functionobjs_misc_stl_comps/algorithms_functionobjs_misc_stl_comps/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  algorithms_functionobjs_misc_stl_comps
+//
+//  Created by Sudheendra Ayalasomayajula on 6/25/13.
+//  Copyright (c) 2013 Sudheendra Ayalasomayajula. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
